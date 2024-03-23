ae72bb4e3971242887aa587109aec5677538f9dc
chris cunningham <crcunningham@users.noreply.github.com>
2024-03-22 23:32:05 -0700

    add custom type support

--- Sources/XcodeProj/Objects/Targets/PBXProductType.swift
+++ Sources/XcodeProj/Objects/Targets/PBXProductType.swift
@@ -1,74 +1,197 @@
 import Foundation
 
-public enum PBXProductType: String, Decodable {
-    case none = ""
-    case application = "com.apple.product-type.application"
-    case framework = "com.apple.product-type.framework"
-    case staticFramework = "com.apple.product-type.framework.static"
-    case xcFramework = "com.apple.product-type.xcframework"
-    case dynamicLibrary = "com.apple.product-type.library.dynamic"
-    case staticLibrary = "com.apple.product-type.library.static"
-    case bundle = "com.apple.product-type.bundle"
-    case unitTestBundle = "com.apple.product-type.bundle.unit-test"
-    case uiTestBundle = "com.apple.product-type.bundle.ui-testing"
-    case appExtension = "com.apple.product-type.app-extension"
-    case extensionKitExtension = "com.apple.product-type.extensionkit-extension"
-    case commandLineTool = "com.apple.product-type.tool"
-    case watchApp = "com.apple.product-type.application.watchapp"
-    case watch2App = "com.apple.product-type.application.watchapp2"
-    case watch2AppContainer = "com.apple.product-type.application.watchapp2-container"
-    case watchExtension = "com.apple.product-type.watchkit-extension"
-    case watch2Extension = "com.apple.product-type.watchkit2-extension"
-    case tvExtension = "com.apple.product-type.tv-app-extension"
-    case messagesApplication = "com.apple.product-type.application.messages"
-    case messagesExtension = "com.apple.product-type.app-extension.messages"
-    case stickerPack = "com.apple.product-type.app-extension.messages-sticker-pack"
-    case xpcService = "com.apple.product-type.xpc-service"
-    case ocUnitTestBundle = "com.apple.product-type.bundle.ocunit-test"
-    case xcodeExtension = "com.apple.product-type.xcode-extension"
-    case instrumentsPackage = "com.apple.product-type.instruments-package"
-    case intentsServiceExtension = "com.apple.product-type.app-extension.intents-service"
-    case onDemandInstallCapableApplication = "com.apple.product-type.application.on-demand-install-capable"
-    case metalLibrary = "com.apple.product-type.metal-library"
-    case driverExtension = "com.apple.product-type.driver-extension"
-    case systemExtension = "com.apple.product-type.system-extension"
+public struct PBXProductType: RawRepresentable, Decodable, Equatable, Hashable {
+  public var rawValue: String
 
-    /// Returns the file extension for the given product type.
-    public var fileExtension: String? {
-        switch self {
-        case .application, .watchApp, .watch2App, .watch2AppContainer, .messagesApplication, .onDemandInstallCapableApplication:
-            return "app"
-        case .framework, .staticFramework:
-            return "framework"
-        case .dynamicLibrary:
-            return "dylib"
-        case .staticLibrary:
-            return "a"
-        case .bundle:
-            return "bundle"
-        case .unitTestBundle, .uiTestBundle:
-            return "xctest"
-        case .appExtension, .extensionKitExtension, .tvExtension, .watchExtension, .watch2Extension, .messagesExtension, .stickerPack, .xcodeExtension,
-                .intentsServiceExtension:
-            return "appex"
-        case .commandLineTool:
-            return nil
-        case .xpcService:
-            return "xpc"
-        case .ocUnitTestBundle:
-            return "octest"
-        case .instrumentsPackage:
-            return "instrpkg"
-        case .xcFramework:
-            return "xcframework"
-        case .metalLibrary:
-            return "metallib"
-        case .systemExtension:
-            return "systemextension"
-        case .driverExtension:
-            return "dext"
-        case .none:
-            return nil
-        }
+  public static let none = PBXProductType(rawValue: "")
+  public static let application = PBXProductType(rawValue: "com.apple.product-type.application")
+  public static let framework = PBXProductType(rawValue: "com.apple.product-type.framework")
+  public static let staticFramework = PBXProductType(
+    rawValue: "com.apple.product-type.framework.static")
+  public static let xcFramework = PBXProductType(rawValue: "com.apple.product-type.xcframework")
+  public static let dynamicLibrary = PBXProductType(
+    rawValue: "com.apple.product-type.library.dynamic")
+  public static let staticLibrary = PBXProductType(
+    rawValue: "com.apple.product-type.library.static")
+  public static let bundle = PBXProductType(rawValue: "com.apple.product-type.bundle")
+  public static let unitTestBundle = PBXProductType(
+    rawValue: "com.apple.product-type.bundle.unit-test")
+  public static let uiTestBundle = PBXProductType(
+    rawValue: "com.apple.product-type.bundle.ui-testing")
+  public static let appExtension = PBXProductType(rawValue: "com.apple.product-type.app-extension")
+  public static let extensionKitExtension = PBXProductType(
+    rawValue: "com.apple.product-type.extensionkit-extension")
+  public static let commandLineTool = PBXProductType(rawValue: "com.apple.product-type.tool")
+  public static let watchApp = PBXProductType(
+    rawValue: "com.apple.product-type.application.watchapp")
+  public static let watch2App = PBXProductType(
+    rawValue: "com.apple.product-type.application.watchapp2")
+  public static let watch2AppContainer = PBXProductType(
+    rawValue: "com.apple.product-type.application.watchapp2-container")
+  public static let watchExtension = PBXProductType(
+    rawValue: "com.apple.product-type.watchkit-extension")
+  public static let watch2Extension = PBXProductType(
+    rawValue: "com.apple.product-type.watchkit2-extension")
+  public static let tvExtension = PBXProductType(
+    rawValue: "com.apple.product-type.tv-app-extension")
+  public static let messagesApplication = PBXProductType(
+    rawValue: "com.apple.product-type.application.messages")
+  public static let messagesExtension = PBXProductType(
+    rawValue: "com.apple.product-type.app-extension.messages")
+  public static let stickerPack = PBXProductType(
+    rawValue: "com.apple.product-type.app-extension.messages-sticker-pack")
+  public static let xpcService = PBXProductType(rawValue: "com.apple.product-type.xpc-service")
+  public static let ocUnitTestBundle = PBXProductType(
+    rawValue: "com.apple.product-type.bundle.ocunit-test")
+  public static let xcodeExtension = PBXProductType(
+    rawValue: "com.apple.product-type.xcode-extension")
+  public static let instrumentsPackage = PBXProductType(
+    rawValue: "com.apple.product-type.instruments-package")
+  public static let intentsServiceExtension = PBXProductType(
+    rawValue: "com.apple.product-type.app-extension.intents-service")
+  public static let onDemandInstallCapableApplication = PBXProductType(
+    rawValue: "com.apple.product-type.application.on-demand-install-capable")
+  public static let metalLibrary = PBXProductType(rawValue: "com.apple.product-type.metal-library")
+  public static let driverExtension = PBXProductType(
+    rawValue: "com.apple.product-type.driver-extension")
+  public static let systemExtension = PBXProductType(
+    rawValue: "com.apple.product-type.system-extension")
+
+  public static let knownTypes: [PBXProductType] = [
+    .none,
+    .application,
+    .framework,
+    .staticFramework,
+    .xcFramework,
+    .dynamicLibrary,
+    .staticLibrary,
+    .bundle,
+    .unitTestBundle,
+    .uiTestBundle,
+    .appExtension,
+    .extensionKitExtension,
+    .commandLineTool,
+    .watchApp,
+    .watch2App,
+    .watch2AppContainer,
+    .watchExtension,
+    .watch2Extension,
+    .tvExtension,
+    .messagesApplication,
+    .messagesExtension,
+    .stickerPack,
+    .xpcService,
+    .ocUnitTestBundle,
+    .xcodeExtension,
+    .instrumentsPackage,
+    .intentsServiceExtension,
+    .onDemandInstallCapableApplication,
+    .metalLibrary,
+    .driverExtension,
+    .systemExtension,
+  ]
+
+  public init(rawValue: String) {
+    self.rawValue = rawValue
+  }
+
+  public static func == (_ lhs: Self, _ rhs: Self) -> Bool {
+    return lhs.rawValue == rhs.rawValue
+  }
+
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(rawValue)
+  }
+
+  // MARK: - Custom Product Types
+  private struct PBXCustomProductType: Hashable {
+    var productType: PBXProductType
+    var fileExtension: String?
+
+    static func == (_ lhs: Self, _ rhs: Self) -> Bool {
+      return lhs.productType == rhs.productType && lhs.fileExtension == rhs.fileExtension
     }
+
+    func hash(into hasher: inout Hasher) {
+      hasher.combine(productType)
+      hasher.combine(fileExtension)
+    }
+  }
+
+  private static let _customTypes = StateLock<Set<PBXCustomProductType>>([])
+
+  /// Adds a custom product type to the list of known types. Thread-safe.
+  public static func addCustomType(_ type: PBXProductType, fileExtension: String?) {
+    guard !knownTypes.contains(type) else { return }
+
+    let customType = PBXCustomProductType(productType: type, fileExtension: fileExtension)
+    _customTypes.withLock {
+      $0.insert(customType)
+    }
+  }
+
+  /// Returns the file extension for the given product type.
+  public var fileExtension: String? {
+    switch self {
+    case PBXProductType.application, PBXProductType.watchApp, PBXProductType.watch2App,
+      PBXProductType.watch2AppContainer, PBXProductType.messagesApplication,
+      PBXProductType.onDemandInstallCapableApplication:
+      return "app"
+    case PBXProductType.framework, PBXProductType.staticFramework:
+      return "framework"
+    case PBXProductType.dynamicLibrary:
+      return "dylib"
+    case PBXProductType.staticLibrary:
+      return "a"
+    case PBXProductType.bundle:
+      return "bundle"
+    case PBXProductType.unitTestBundle, PBXProductType.uiTestBundle:
+      return "xctest"
+    case PBXProductType.appExtension, PBXProductType.extensionKitExtension,
+      PBXProductType.tvExtension, PBXProductType.watchExtension, PBXProductType.watch2Extension,
+      PBXProductType.messagesExtension, PBXProductType.stickerPack, PBXProductType.xcodeExtension,
+      PBXProductType.intentsServiceExtension:
+      return "appex"
+    case PBXProductType.commandLineTool:
+      return nil
+    case PBXProductType.xpcService:
+      return "xpc"
+    case PBXProductType.ocUnitTestBundle:
+      return "octest"
+    case PBXProductType.instrumentsPackage:
+      return "instrpkg"
+    case PBXProductType.xcFramework:
+      return "xcframework"
+    case PBXProductType.metalLibrary:
+      return "metallib"
+    case PBXProductType.systemExtension:
+      return "systemextension"
+    case PBXProductType.driverExtension:
+      return "dext"
+    case PBXProductType.none:
+      return nil
+    default:
+      // fall back to a custom type
+      let custom = Self._customTypes.withLock { $0 }
+      return custom.first { $0.productType == self }?.fileExtension
+    }
+  }
+}
+
+// MARK: - StateLock
+
+private class StateLock<T> {
+  private var state: T
+  private let lock = NSLock()
+
+  init(_ state: T) {
+    self.state = state
+  }
+
+  @discardableResult
+  func withLock<R>(_ block: (inout T) -> R) -> R {
+    return self.lock.withLock {
+      return block(&self.state)
+    }
+  }
 }
--- Tests/XcodeProjTests/Objects/Targets/PBXProductTypeTests.swift
+++ Tests/XcodeProjTests/Objects/Targets/PBXProductTypeTests.swift
@@ -1,109 +1,238 @@
 import Foundation
-import XcodeProj
 import XCTest
+import XcodeProj
 
 final class PBXProductTypeTests: XCTestCase {
-    func test_none_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.none.rawValue, "")
-    }
 
-    func test_application_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.application.rawValue, "com.apple.product-type.application")
-    }
+  // MARK: - Product type strings
+  func test_none_hasTheRightValue() {
+    XCTAssertEqual(PBXProductType.none.rawValue, "")
+  }
 
-    func test_framework_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.framework.rawValue, "com.apple.product-type.framework")
-    }
+  func test_application_hasTheRightValue() {
+    XCTAssertEqual(PBXProductType.application.rawValue, "com.apple.product-type.application")
+  }
 
-    func test_dynamicLibrary_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.dynamicLibrary.rawValue, "com.apple.product-type.library.dynamic")
-    }
+  func test_framework_hasTheRightValue() {
+    XCTAssertEqual(PBXProductType.framework.rawValue, "com.apple.product-type.framework")
+  }
 
-    func test_staticLibrary_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.staticLibrary.rawValue, "com.apple.product-type.library.static")
-    }
+  func test_dynamicLibrary_hasTheRightValue() {
+    XCTAssertEqual(PBXProductType.dynamicLibrary.rawValue, "com.apple.product-type.library.dynamic")
+  }
 
-    func test_bundle_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.bundle.rawValue, "com.apple.product-type.bundle")
-    }
+  func test_staticLibrary_hasTheRightValue() {
+    XCTAssertEqual(PBXProductType.staticLibrary.rawValue, "com.apple.product-type.library.static")
+  }
 
-    func test_unitTestBundle_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.unitTestBundle.rawValue, "com.apple.product-type.bundle.unit-test")
-    }
+  func test_bundle_hasTheRightValue() {
+    XCTAssertEqual(PBXProductType.bundle.rawValue, "com.apple.product-type.bundle")
+  }
 
-    func test_uiTestBundle_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.uiTestBundle.rawValue, "com.apple.product-type.bundle.ui-testing")
-    }
+  func test_unitTestBundle_hasTheRightValue() {
+    XCTAssertEqual(
+      PBXProductType.unitTestBundle.rawValue, "com.apple.product-type.bundle.unit-test")
+  }
 
-    func test_appExtension_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.appExtension.rawValue, "com.apple.product-type.app-extension")
-    }
+  func test_uiTestBundle_hasTheRightValue() {
+    XCTAssertEqual(PBXProductType.uiTestBundle.rawValue, "com.apple.product-type.bundle.ui-testing")
+  }
 
-    func test_extensionKitExtension_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.extensionKitExtension.rawValue, "com.apple.product-type.extensionkit-extension")
-    }
+  func test_appExtension_hasTheRightValue() {
+    XCTAssertEqual(PBXProductType.appExtension.rawValue, "com.apple.product-type.app-extension")
+  }
 
-    func test_commandLineTool_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.commandLineTool.rawValue, "com.apple.product-type.tool")
-    }
+  func test_extensionKitExtension_hasTheRightValue() {
+    XCTAssertEqual(
+      PBXProductType.extensionKitExtension.rawValue, "com.apple.product-type.extensionkit-extension"
+    )
+  }
 
-    func test_watchApp_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.watchApp.rawValue, "com.apple.product-type.application.watchapp")
-    }
+  func test_commandLineTool_hasTheRightValue() {
+    XCTAssertEqual(PBXProductType.commandLineTool.rawValue, "com.apple.product-type.tool")
+  }
 
-    func test_watch2App_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.watch2App.rawValue, "com.apple.product-type.application.watchapp2")
-    }
+  func test_watchApp_hasTheRightValue() {
+    XCTAssertEqual(PBXProductType.watchApp.rawValue, "com.apple.product-type.application.watchapp")
+  }
 
-    func test_watchExtension_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.watchExtension.rawValue, "com.apple.product-type.watchkit-extension")
-    }
+  func test_watch2App_hasTheRightValue() {
+    XCTAssertEqual(
+      PBXProductType.watch2App.rawValue, "com.apple.product-type.application.watchapp2")
+  }
 
-    func test_watch2Extension_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.watch2Extension.rawValue, "com.apple.product-type.watchkit2-extension")
-    }
+  func test_watchExtension_hasTheRightValue() {
+    XCTAssertEqual(
+      PBXProductType.watchExtension.rawValue, "com.apple.product-type.watchkit-extension")
+  }
 
-    func test_tvExtension_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.tvExtension.rawValue, "com.apple.product-type.tv-app-extension")
-    }
+  func test_watch2Extension_hasTheRightValue() {
+    XCTAssertEqual(
+      PBXProductType.watch2Extension.rawValue, "com.apple.product-type.watchkit2-extension")
+  }
 
-    func test_messagesApplication_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.messagesApplication.rawValue, "com.apple.product-type.application.messages")
-    }
+  func test_tvExtension_hasTheRightValue() {
+    XCTAssertEqual(PBXProductType.tvExtension.rawValue, "com.apple.product-type.tv-app-extension")
+  }
 
-    func test_messagesExtension_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.messagesExtension.rawValue, "com.apple.product-type.app-extension.messages")
-    }
+  func test_messagesApplication_hasTheRightValue() {
+    XCTAssertEqual(
+      PBXProductType.messagesApplication.rawValue, "com.apple.product-type.application.messages")
+  }
 
-    func test_stickerPack_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.stickerPack.rawValue, "com.apple.product-type.app-extension.messages-sticker-pack")
-    }
+  func test_messagesExtension_hasTheRightValue() {
+    XCTAssertEqual(
+      PBXProductType.messagesExtension.rawValue, "com.apple.product-type.app-extension.messages")
+  }
 
-    func test_xpcService_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.xpcService.rawValue, "com.apple.product-type.xpc-service")
-    }
+  func test_stickerPack_hasTheRightValue() {
+    XCTAssertEqual(
+      PBXProductType.stickerPack.rawValue,
+      "com.apple.product-type.app-extension.messages-sticker-pack")
+  }
 
-    func test_ocUnitTestBundle_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.ocUnitTestBundle.rawValue, "com.apple.product-type.bundle.ocunit-test")
-    }
+  func test_xpcService_hasTheRightValue() {
+    XCTAssertEqual(PBXProductType.xpcService.rawValue, "com.apple.product-type.xpc-service")
+  }
 
-    func test_xcodeExtension_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.xcodeExtension.rawValue, "com.apple.product-type.xcode-extension")
-    }
+  func test_ocUnitTestBundle_hasTheRightValue() {
+    XCTAssertEqual(
+      PBXProductType.ocUnitTestBundle.rawValue, "com.apple.product-type.bundle.ocunit-test")
+  }
 
-    func test_intentsServiceExtension_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.intentsServiceExtension.rawValue, "com.apple.product-type.app-extension.intents-service")
-    }
+  func test_xcodeExtension_hasTheRightValue() {
+    XCTAssertEqual(PBXProductType.xcodeExtension.rawValue, "com.apple.product-type.xcode-extension")
+  }
 
-    func test_appClip_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.onDemandInstallCapableApplication.rawValue, "com.apple.product-type.application.on-demand-install-capable")
-    }
+  func test_intentsServiceExtension_hasTheRightValue() {
+    XCTAssertEqual(
+      PBXProductType.intentsServiceExtension.rawValue,
+      "com.apple.product-type.app-extension.intents-service")
+  }
 
-    func test_driverExtension_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.driverExtension.rawValue, "com.apple.product-type.driver-extension")
-    }
+  func test_appClip_hasTheRightValue() {
+    XCTAssertEqual(
+      PBXProductType.onDemandInstallCapableApplication.rawValue,
+      "com.apple.product-type.application.on-demand-install-capable")
+  }
 
-    func test_systemExtension_hasTheRightValue() {
-        XCTAssertEqual(PBXProductType.systemExtension.rawValue, "com.apple.product-type.system-extension")
-    }
+  func test_driverExtension_hasTheRightValue() {
+    XCTAssertEqual(
+      PBXProductType.driverExtension.rawValue, "com.apple.product-type.driver-extension")
+  }
+
+  func test_systemExtension_hasTheRightValue() {
+    XCTAssertEqual(
+      PBXProductType.systemExtension.rawValue, "com.apple.product-type.system-extension")
+  }
+
+  // MARK: - File extension
+  func test_fileExtension_knownTypes() {
+    XCTAssertEqual(PBXProductType.application.fileExtension, "app")
+    XCTAssertEqual(PBXProductType.framework.fileExtension, "framework")
+    XCTAssertEqual(PBXProductType.dynamicLibrary.fileExtension, "dylib")
+    XCTAssertEqual(PBXProductType.staticLibrary.fileExtension, "a")
+    XCTAssertEqual(PBXProductType.bundle.fileExtension, "bundle")
+    XCTAssertEqual(PBXProductType.unitTestBundle.fileExtension, "xctest")
+    XCTAssertEqual(PBXProductType.uiTestBundle.fileExtension, "xctest")
+    XCTAssertEqual(PBXProductType.appExtension.fileExtension, "appex")
+    XCTAssertEqual(PBXProductType.extensionKitExtension.fileExtension, "appex")
+    XCTAssertEqual(PBXProductType.commandLineTool.fileExtension, nil)
+    XCTAssertEqual(PBXProductType.watchApp.fileExtension, "app")
+    XCTAssertEqual(PBXProductType.watch2App.fileExtension, "app")
+    XCTAssertEqual(PBXProductType.watchExtension.fileExtension, "appex")
+    XCTAssertEqual(PBXProductType.watch2Extension.fileExtension, "appex")
+    XCTAssertEqual(PBXProductType.tvExtension.fileExtension, "appex")
+    XCTAssertEqual(PBXProductType.messagesApplication.fileExtension, "app")
+    XCTAssertEqual(PBXProductType.messagesExtension.fileExtension, "appex")
+    XCTAssertEqual(PBXProductType.stickerPack.fileExtension, "appex")
+    XCTAssertEqual(PBXProductType.xpcService.fileExtension, "xpc")
+    XCTAssertEqual(PBXProductType.ocUnitTestBundle.fileExtension, "octest")
+    XCTAssertEqual(PBXProductType.xcodeExtension.fileExtension, "appex")
+    XCTAssertEqual(PBXProductType.intentsServiceExtension.fileExtension, "appex")
+    XCTAssertEqual(PBXProductType.onDemandInstallCapableApplication.fileExtension, "app")
+    XCTAssertEqual(PBXProductType.driverExtension.fileExtension, "dext")
+    XCTAssertEqual(PBXProductType.systemExtension.fileExtension, "systemextension")
+    XCTAssertEqual(PBXProductType.xcFramework.fileExtension, "xcframework")
+    XCTAssertEqual(PBXProductType.metalLibrary.fileExtension, "metallib")
+    XCTAssertEqual(PBXProductType.instrumentsPackage.fileExtension, "instrpkg")
+    XCTAssertEqual(PBXProductType.none.fileExtension, nil)
+  }
+
+  // MARK: - Init
+  func test_init_knownTypes() {
+    XCTAssertEqual(PBXProductType(rawValue: ""), .none)
+    XCTAssertEqual(PBXProductType(rawValue: "com.apple.product-type.application"), .application)
+    XCTAssertEqual(PBXProductType(rawValue: "com.apple.product-type.framework"), .framework)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.framework.static"), .staticFramework)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.xcframework"), .xcFramework)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.library.dynamic"), .dynamicLibrary)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.library.static"), .staticLibrary)
+    XCTAssertEqual(PBXProductType(rawValue: "com.apple.product-type.bundle"), .bundle)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.bundle.unit-test"), .unitTestBundle)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.bundle.ui-testing"), .uiTestBundle)
+    XCTAssertEqual(PBXProductType(rawValue: "com.apple.product-type.app-extension"), .appExtension)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.extensionkit-extension"),
+      .extensionKitExtension)
+    XCTAssertEqual(PBXProductType(rawValue: "com.apple.product-type.tool"), .commandLineTool)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.application.watchapp"), .watchApp)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.application.watchapp2"), .watch2App)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.application.watchapp2-container"),
+      .watch2AppContainer)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.watchkit-extension"), .watchExtension)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.watchkit2-extension"), .watch2Extension)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.tv-app-extension"), .tvExtension)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.application.messages"), .messagesApplication)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.app-extension.messages"), .messagesExtension)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.app-extension.messages-sticker-pack"),
+      .stickerPack)
+    XCTAssertEqual(PBXProductType(rawValue: "com.apple.product-type.xpc-service"), .xpcService)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.bundle.ocunit-test"), .ocUnitTestBundle)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.xcode-extension"), .xcodeExtension)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.instruments-package"), .instrumentsPackage)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.app-extension.intents-service"),
+      .intentsServiceExtension)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.application.on-demand-install-capable"),
+      .onDemandInstallCapableApplication)
+    XCTAssertEqual(PBXProductType(rawValue: "com.apple.product-type.metal-library"), .metalLibrary)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.driver-extension"), .driverExtension)
+    XCTAssertEqual(
+      PBXProductType(rawValue: "com.apple.product-type.system-extension"), .systemExtension)
+  }
+
+  func test_init_unknownType() {
+    XCTAssertNotNil(PBXProductType(rawValue: "com.apple.product-type.beeper"))
+    XCTAssertEqual(PBXProductType(rawValue: "com.apple.product-type.beeper").fileExtension, nil)
+  }
+
+  func test_add_customType() {
+    let customType = PBXProductType(rawValue: "com.apple.product-type.beeper")
+    XCTAssertNil(customType)
+    PBXProductType.addCustomType(customType, fileExtension: "beep")
+
+    XCTAssertEqual(PBXProductType(rawValue: "com.apple.product-type.beeper"), customType)
+    XCTAssertEqual(PBXProductType(rawValue: "com.apple.product-type.beeper").fileExtension, "beep")
+  }
 }
